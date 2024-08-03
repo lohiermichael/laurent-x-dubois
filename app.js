0fb533ff085d42aaa1b6c623f2487024cf87ec64
Michael Lohier <lohiermichael@gmail.com>
2024-08-03 12:19:32 +0300

    Make the 404 page work
    Signed-off-by: Michael Lohier <lohiermichael@gmail.com>

--- app.js
+++ app.js
@@ -1,12 +1,49 @@
+// ############################################################################
+// Imports
+// ############################################################################
+
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// ############################################################################
+// Configuration
+// ############################################################################
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
 const app = express();
 
+// Static files
+app.use('/css', express.static(path.join(__dirname, 'public/css')));
+app.use('/img', express.static(path.join(__dirname, 'public/img')));
+app.use('/js', express.static(path.join(__dirname, 'public/js')));
+
+// Set views
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// ############################################################################
+// Statistics
+// ############################################################################
+
+const googleAnalyticsMeasurementId = process.env.GOOGLE_ANALYTICS_MEASUREMENT_ID;
+
+// ############################################################################
+// Routes
+// ############################################################################
+
 app.get('/', (_, res) => {
   res.send('<p>Hello World!</p>');
+});
+
+// Wild card
+app.get('*', (_, response) => {
+    response.render('404', { googleAnalyticsMeasurementId, })
 });
 
 const PORT = process.env.PORT || 3000;
---
+++ public/js/header.js
@@ -0,0 +1,6 @@
+export const elementsToChangeOpen = [
+    document.querySelector('.menu-nav'),
+    document.querySelector('.burger'),
+    document.querySelector('.logo'),
+    document.querySelector('.footer'),
+];
