74e26e37835cac60903439b5acc4fc5c853094ff
Michael Lohier <lohiermichael@gmail.com>
2024-11-27 15:25:08 +0200

    seo: remove En on home title
    Signed-off-by: Michael Lohier <lohiermichael@gmail.com>

--- src/services/seoService.js
+++ src/services/seoService.js
@@ -8,11 +8,10 @@ const {
 class SeoService {
   getMetadataForPath(path, language = 'fr') {
     // Remove language prefix if present
-    const cleanPath = path.replace(/^\/en\//, '/');
+    const cleanPath = path.replace(/^\/en/, '');
 
     // Remove leading slash and get first segment
     const segment = cleanPath.split('/')[1] || 'home';
-
 
     // Get metadata for the current language
     const languageMetadata = pageMetadata[language];
